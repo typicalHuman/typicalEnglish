e7c6503016f586b0af4d949d9d8ca4836da1951b
typicalHuman <reallytovald@gmail.com>
2020-06-18 10:09:38 +0600

    start

---
+++ typicalEnglish/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using typicalEnglish.Scripts.ViewModels;
+
+namespace typicalEnglish
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+
+    public partial class App : Application
+    {
+        public static MainWindowVM MainVM { get; set; }
+        public App()
+        {
+            MainVM = new MainWindowVM();
+        }
+
+    }
+}
---
+++ typicalEnglish/Scripts/ViewModels/MainWindowVM.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace typicalEnglish.Scripts.ViewModels
+{
+    public class MainWindowVM: INotifyPropertyChanged
+    {
+        #region Properties
+
+        #region ResizeMode
+        private ResizeMode resizeMode = ResizeMode.CanResizeWithGrip;
+        public ResizeMode ResizeMode
+        {
+            get => resizeMode;
+            set
+            {
+                resizeMode = value;
+                OnPropertyChanged("ResizeMode");
+            }
+        }
+        #endregion
+
+        #region WindowState
+
+        private WindowState windowState = WindowState.Normal;
+        public WindowState WindowState
+        {
+            get => windowState;
+            set
+            {
+                windowState = value;
+                OnPropertyChanged("WindowState");
+            }
+        }
+
+        #endregion
+
+        #region WindowBorderThickness
+
+        private Thickness windowBorderThickess;
+        public Thickness WindowBorderThickness
+        {
+            get => windowBorderThickess;
+            set
+            {
+                windowBorderThickess = value;
+                OnPropertyChanged("WindowBorderThickness");
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Commands
+
+        #region CloseCommand
+        public Action CloseAction { get; set; }
+        private RelayCommand closeCommand;
+        public RelayCommand CloseCommand
+        {
+            get => closeCommand ?? (closeCommand = new RelayCommand(obj =>
+            {
+                CloseAction();
+            }));
+
+        }
+        #endregion
+
+        #region MaximizeCommand
+        private RelayCommand maximizeCommand;
+        public RelayCommand MaximizeCommand
+        {
+            get => maximizeCommand ?? (maximizeCommand = new RelayCommand(obj =>
+            {
+                if (WindowState == WindowState.Normal)
+                {
+                    //setting border no style
+                    WindowBorderThickness = new Thickness(0);
+                    ResizeMode = ResizeMode.NoResize;
+                    WindowState = WindowState.Maximized;
+                }
+                else
+                {
+                    //setting border to resize with grip
+                    WindowBorderThickness = new Thickness(1);
+                    ResizeMode = ResizeMode.CanResizeWithGrip;
+                    WindowState = WindowState.Normal;
+                }
+            }));
+        }
+        #endregion
+
+        #region MinimizeCommand
+        private RelayCommand minimizeCommand;
+        public RelayCommand MinimizeCommand
+        {
+            get => minimizeCommand ?? (minimizeCommand = new RelayCommand(obj =>
+            {
+                WindowState = WindowState.Minimized;
+            }));
+        }
+        #endregion
+
+        #endregion
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
