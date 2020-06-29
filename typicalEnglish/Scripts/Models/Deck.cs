8ab13e0f57cc7d20f901b37464fc70962230c671
typicalHuman <reallytovald@gmail.com>
2020-06-29 09:21:57 +0600

    decks

---
+++ typicalEnglish/Scripts/Models/Deck.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using typicalEnglish.Scripts.ViewModels;
+
+namespace typicalEnglish.Scripts.Models
+{
+    public class Deck: INotifyPropertyChanged
+    {
+        #region Properties
+        public ObservableCollection<Word> Words { get; set; } = new ObservableCollection<Word>();
+
+        #region ImageSource
+
+        private string imageSource;
+        public string ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged("ImageSource");
+            }
+        }
+
+        #endregion
+
+        #region Name
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        #endregion
+
+        #region WordsCount
+
+        private int wordsCount;
+        public int WordsCount
+        {
+            get => Words.Count;
+            set
+            {
+                wordsCount = value;
+                OnPropertyChanged("WordsCount");
+            }
+        }
+
+        #endregion
+        #endregion
+
+        #region Commands
+
+        #region DeleteCommand
+
+        private RelayCommand deleteCommand;
+        public RelayCommand DeleteCommand
+        {
+            get => deleteCommand ?? (deleteCommand = new RelayCommand(obj =>
+            {
+                App.DeckVM.Decks.Remove(this);
+            }));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
--- typicalEnglish/Scripts/ViewModels/DeckPageVM.cs
+++ typicalEnglish/Scripts/ViewModels/DeckPageVM.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using typicalEnglish.Scripts.Models;
 
 namespace typicalEnglish.Scripts.ViewModels
 {
     public class DeckPageVM: INotifyPropertyChanged
     {
+        #region Properties
+
+        public ObservableCollection<Deck> Decks { get; set; } = new ObservableCollection<Deck>()
+        {
+            new Deck(){Name = "CEEEEEEC"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "CEEEEEEC"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "CEEEEEEC"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "FUUUUUU"},
+            new Deck(){Name = "FUUUUUU"}
+        };
+
+        #endregion
+
+        #region Commands
+
+        #region AddCommand
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get => addCommand ?? (addCommand = new RelayCommand(obj =>
+            {
+                Decks.Insert(0, new Deck());
+            }));
+        }
+        #endregion
+
+
+        #endregion
+
         #region PropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName]string prop = "")
