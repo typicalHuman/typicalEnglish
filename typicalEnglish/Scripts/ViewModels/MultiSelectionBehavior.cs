b5af9bbb0c832ee3c3dfe627328c61d415376bf4
typicalHuman <reallytovald@gmail.com>
2020-07-24 09:13:53 +0600

    listViewItem selection

---
+++ typicalEnglish/Scripts/ViewModels/MultiSelectionBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace typicalEnglish.Scripts.ViewModels
+{
+    public static class MultiSelectionBehavior
+    {
+
+        public static readonly DependencyProperty ClickSelectionProperty =
+            DependencyProperty.RegisterAttached("ClickSelection",
+                                                typeof(bool),
+                                                typeof(MultiSelectionBehavior),
+                                                new UIPropertyMetadata(false, OnClickSelectionChanged));
+        public static bool GetClickSelection(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(ClickSelectionProperty);
+        }
+        public static void SetClickSelection(DependencyObject obj, bool value)
+        {
+            obj.SetValue(ClickSelectionProperty, value);
+        }
+        private static void OnClickSelectionChanged(DependencyObject dpo,
+                                                                 DependencyPropertyChangedEventArgs e)
+        {
+            ListView listView = dpo as ListView;
+            if (listView != null)
+            {
+                if ((bool)e.NewValue == true)
+                {
+                    listView.SelectionChanged += OnSelectionChanged;
+                }
+                else
+                {
+                    listView.SelectionChanged -= OnSelectionChanged;
+                }
+            }
+        }
+        static void OnSelectionChanged(object sender, SelectionChangedEventArgs e) { }
+    }
+}
