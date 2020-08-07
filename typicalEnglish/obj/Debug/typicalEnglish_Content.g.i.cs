92efce4e8953a048fe27b377bd7e4d5fa9a6378a
typicalHuman <reallytovald@gmail.com>
2020-08-07 09:50:48 +0600

    gifs

--- typicalEnglish/obj/Debug/typicalEnglish_Content.g.i.cs
+++ typicalEnglish/obj/Debug/typicalEnglish_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("language_www_icon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/gifs/fun.gif")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/gifs/sad.gif")]
 
 
